0bb5f814baedcc08807db829c4398f266b58ea69
thembones79 <michal.szumnarski@gmail.com>
2019-05-24 20:30:02 +0200

    first commit

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const app = express();
+const bcrypt = require("bcrypt");
 
 fccTesting(app); //For FCC testing purposes
 
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
+app.use("/", (req, res) => {
+  res.send("<b>BCrypt Challenges</b>");
+});
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(hash);
+console.log(result);
 
 //END_SYNC
-
 
 app.listen(process.env.PORT || 3000, () => {});
